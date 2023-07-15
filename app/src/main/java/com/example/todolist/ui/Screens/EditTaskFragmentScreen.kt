ca2f9c22cab4d3cad7cda763fcefffd30f4d95f3
Flanozzz <belkapult@yandex.ru>
2023-07-15 17:47:05 +0300

    add header elevation

--- app/src/main/java/com/example/todolist/ui/Screens/EditTaskFragmentScreen.kt
+++ app/src/main/java/com/example/todolist/ui/Screens/EditTaskFragmentScreen.kt
@@ -1,8 +1,13 @@
 package com.example.todolist.ui.Screens
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,14 +15,25 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Close
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.todolist.R
 import com.example.todolist.data.model.ToDoItem
 import com.example.todolist.ui.Screens.actions.EditTaskScreenActions
 import com.example.todolist.ui.Screens.components.DeadlinePicker
@@ -29,6 +45,7 @@ import com.example.todolist.ui.Screens.components.Header
 import com.example.todolist.ui.Screens.components.ImportanceSection
 import com.example.todolist.ui.Screens.components.BottomSheetContent
 import com.example.todolist.ui.theme.AppTheme
+import com.example.todolist.ui.theme.button
 import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
 
 
@@ -48,61 +65,68 @@ fun Screen(
         )
         val scrollState = rememberScrollState()
         val highlightedState = remember{mutableStateOf(false)}
-        ModalBottomSheetLayout(
-            sheetState = modalBottomSheetState,
-            sheetContent = { BottomSheetContent(
-                bottomSheetState = modalBottomSheetState,
-                updateImportanceAction = actions.updateImportanceAction,
-                highlightedState
-            )
-            },
-            sheetShape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
-        ) {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(scrollState)
-            ){
-                Column {
-                    Header(
-                        goBackAction = actions.goBackAction,
-                        saveButtonAction = actions.saveButtonAction
-                    )
-                    Column(
-                        modifier = Modifier
-                            .padding(start = 16.dp, end = 16.dp)
-                    ) {
-                        EditField(
-                            text = selectedTask.value.text,
-                            updateTextAction = actions.updateTextAction
+        val toolbarElevation by animateDpAsState(
+            targetValue = if (scrollState.value > 0) 20.dp else 0.dp
+        )
+        Surface() {
+            ModalBottomSheetLayout(
+                sheetState = modalBottomSheetState,
+                sheetContent = { BottomSheetContent(
+                    bottomSheetState = modalBottomSheetState,
+                    updateImportanceAction = actions.updateImportanceAction,
+                    highlightedState
+                )
+                },
+                sheetShape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+            ) {
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                ){
+                    Column {
+                        Header(
+                            goBackAction = actions.goBackAction,
+                            saveButtonAction = actions.saveButtonAction,
+                            toolbarElevation = toolbarElevation
                         )
-                        ImportanceSection(
-                            bottomSheetState = modalBottomSheetState,
-                            importanceText = actions.getStringByImportanceAction(
-                                selectedTask.value.importance
-                            ),
-                            hideKeyboardAction = actions.hideKeyboardAction,
-                            highlightedState = highlightedState
-                        )
-                        Divider(
-                            Modifier.padding(top = 16.dp, bottom = 16.dp)
-                        )
-                        DeadlineSection(
-                            selectedTask = selectedTask.value,
-                            calendarState = calendarState,
-                            clockState = clockState,
-                            updateDeadlineAction = actions.updateDeadlineAction
-                        )
+                        Column(
+                            modifier = Modifier
+                                .padding(start = 16.dp, end = 16.dp)
+                                .verticalScroll(scrollState)
+                        ) {
+                            EditField(
+                                text = selectedTask.value.text,
+                                updateTextAction = actions.updateTextAction
+                            )
+                            ImportanceSection(
+                                bottomSheetState = modalBottomSheetState,
+                                importanceText = actions.getStringByImportanceAction(
+                                    selectedTask.value.importance
+                                ),
+                                hideKeyboardAction = actions.hideKeyboardAction,
+                                highlightedState = highlightedState
+                            )
+                            Divider(
+                                Modifier.padding(top = 16.dp, bottom = 16.dp)
+                            )
+                            DeadlineSection(
+                                selectedTask = selectedTask.value,
+                                calendarState = calendarState,
+                                clockState = clockState,
+                                updateDeadlineAction = actions.updateDeadlineAction
+                            )
+                            Divider(
+                                Modifier.padding(top = 16.dp, bottom = 16.dp)
+                            )
+                            DeleteTaskButton(actions.deleteButtonClickAction, isNewTask = isNewTask)
+                            Row(modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp)) {}
+                        }
                     }
-                    Divider(
-                        Modifier.padding(top = 16.dp, bottom = 16.dp)
-                    )
-                    DeleteTaskButton(actions.deleteButtonClickAction, isNewTask = isNewTask)
                 }
-            }
 
+            }
+            DeadlineTimePicker(clockState = clockState, clockSelection = actions.clockSelection)
+            DeadlinePicker(calendarState, actions.calendarSelection)
         }
-        DeadlineTimePicker(clockState = clockState, clockSelection = actions.clockSelection)
-        DeadlinePicker(calendarState, actions.calendarSelection)
     }
 }
 
--- app/src/main/java/com/example/todolist/ui/Screens/components/Header.kt
+++ app/src/main/java/com/example/todolist/ui/Screens/components/Header.kt
@@ -1,20 +1,28 @@
 package com.example.todolist.ui.Screens.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.todolist.R
 import com.example.todolist.ui.theme.button
@@ -23,29 +31,34 @@ import com.example.todolist.ui.theme.button
 @Preview
 fun Header(
     goBackAction: () -> Unit = {},
-    saveButtonAction: () -> Unit = {}
+    saveButtonAction: () -> Unit = {},
+    toolbarElevation: Dp = 0.dp
 ){
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+    Surface(
+        shadowElevation = toolbarElevation
     ) {
-        IconButton(
-            onClick = { goBackAction() }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
         ) {
-            Icon(
-                imageVector = Icons.Sharp.Close,
-                contentDescription = "close",
-                tint = MaterialTheme.colorScheme.primary
-            )
-        }
-        TextButton(
-            modifier = Modifier.padding(end = 4.dp),
-            onClick = { saveButtonAction() }
-        ) {
-            Text(
-                text = stringResource(id = R.string.save),
-                style = button
-            )
+            IconButton(
+                onClick = { goBackAction() }
+            ) {
+                Icon(
+                    imageVector = Icons.Sharp.Close,
+                    contentDescription = "close",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+            TextButton(
+                modifier = Modifier.padding(end = 4.dp),
+                onClick = { saveButtonAction() }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.save),
+                    style = button
+                )
+            }
         }
     }
 }
